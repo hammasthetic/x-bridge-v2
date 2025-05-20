f2b99c987a4d2a823d854e031b3440b9a53883c9
hammasxd <142976137+hammasxd@users.noreply.github.com>
2025-05-20 07:57:33 -0700

    justify

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,8 +12,8 @@ function Header() {
   ];
 
   return (
-    <div className="w-full flex flex-col items-end al justify-end sm:flex-row gap-4 sm:gap-6 md:gap-8">
-      <div className="navbar w-full flex flex-col sm:flex-row rounded-sm sm:rounded-md justify-between bg-content1 py-2 sm:py-3 md:py-4 px-4 sm:px-5 md:px-6">
+    <div className="w-full flex flex-col items-end  justify-end sm:flex-row gap-4 sm:gap-6 md:gap-8">
+      <div className="navbar w-full justify-end flex flex-col sm:flex-row rounded-sm sm:rounded-md  bg-content1 py-2 sm:py-3 md:py-4 px-4 sm:px-5 md:px-6">
         <div className="chainAndWallet flex flex-col gap-3 sm:flex-row sm:gap-2 md:gap-2 items-center">
           <ActiveChainSwitcher />
           <div className="flex flex-row gap-2 justify-center items-center align-middle">
